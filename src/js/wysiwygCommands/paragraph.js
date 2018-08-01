dbfb1d0e64078a258391c6747bde16a411c03e22
jinlong <long@blocks.tech>
2018-08-01 18:49:00 +0800

    cancel gotop when add heading in wysiwyg

--- src/js/wysiwygCommands/paragraph.js
+++ src/js/wysiwygCommands/paragraph.js
@@ -21,7 +21,7 @@ const Paragraph = CommandManager.command('wysiwyg', /** @lends Paragraph */{
   exec(wwe) {
     const sq = wwe.getEditor();
 
-    wwe.focus();
+    // wwe.focus();
 
     if (!sq.hasFormat('TABLE') && !sq.hasFormat('PRE')) {
       sq.modifyBlocks(fragment => {
